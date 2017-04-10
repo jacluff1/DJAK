4d4b7291e416b1ddf8c24711cd054cb07e7aa56f
jacluff1 <jacluff1@asu.edu>
2017-04-09 18:30:41 -0700

    added .gitignore, removed **.pyc files and __pycached__/**, modified fits.lin_fit to return dictionary

--- fits.py
+++ fits.py
@@ -4,7 +4,7 @@ from scipy import optimize
 __all__ = ['SS_xx','SS_xy','m_exp','b_exp','SS_yy','SS_E','SS_R',
            'sig_y','sig_m','sig_b','RMSE','R2','lin_fit','least_square']
 
-def SS_xx(X): # 
+def SS_xx(X): #
 	x_bar = np.average(X)
 	sum = 0
 	for x in X:
@@ -118,23 +118,27 @@ def lin_fit(X_data,Y_data,X_lin):
     Y_data=np.array(Y_data)
     m = m_exp(X_data,Y_data)
     b = b_exp(X_data,Y_data)
-    Y_lin = m*X_lin + b
-    
-    #m_err = np.sqrt(s_m2(X_data,Y_data))
-    #b_err = np.sqrt(s_b2(X_data,Y_data))
-    #Yp = (m+m_err)*X_lin + (b+b_err)
-    #Ym = (m-m_err)*X_lin + (b-b_err)
-    return Y_lin
+    Y_fit = m*X_lin + b
+
+    m_err = sig_m(X_data,Y_data)
+    b_err = sig_b(X_data,Y_data)
+
+    m_var = dg.var(m,m_err)
+    b_var = dg.var(b,b_err)
+
+    R2 = R2(X_data,Y_data)
+
+    return {'Y_fit':Y_fit, 'm':m_var, 'b':b_var, 'R2':R2}
 
 def least_square(fitfunc,par,X_data,Y_data,cycles=5000):
-    
+
     def errfunc(p,x,y):
         return y - fitfunc(p,x)
     xdata = np.array(X_data)
     ydata = np.array(Y_data)
-    
+
     qout,success = optimize.leastsq(errfunc,par,args=(xdata,ydata),maxfev=cycles)
-    
+
     if success == False:
         print("OMFG - Failed!!")
     return qout
@@ -157,22 +161,22 @@ def least_square(fitfunc,par,X_data,Y_data,cycles=5000):
 #     else:
 #         pcov = np.inf
 
-#     error = [] 
+#     error = []
 #     for i in range(len(pfit)):
 #         try:
 #           error.append(np.absolute(pcov[i][i])**0.5)
 #         except:
 #           error.append( 0.00 )
 #     pfit_leastsq = pfit
-#     perr_leastsq = np.array(error) 
-#     return pfit_leastsq, perr_leastsq 
+#     perr_leastsq = np.array(error)
+#     return pfit_leastsq, perr_leastsq
 
 # def fit_curvefit(p0, datax, datay, function, yerr=err_stdev, **kwargs):
 
 #     pfit, pcov = \
 #          optimize.curve_fit(f,datax,datay,p0=p0,\
 #                             sigma=yerr, epsfcn=0.0001, **kwargs)
-#     error = [] 
+#     error = []
 #     for i in range(len(pfit)):
 #         try:
 #           error.append(np.absolute(pcov[i][i])**0.5)
@@ -180,8 +184,4 @@ def least_square(fitfunc,par,X_data,Y_data,cycles=5000):
 #           error.append( 0.00 )
 #     pfit_curvefit = pfit
 #     perr_curvefit = np.array(error)
-#     return pfit_curvefit, perr_curvefit 
-    
-
-
-
+#     return pfit_curvefit, perr_curvefit
--- gen.py
+++ gen.py
@@ -1,23 +1,99 @@
 import numpy as np
+import pandas as pd
 import csv
 
 __all__ = ['CSV','sort','nearest','maxima','minima','extrema',
            'var']
 
-def CSV(path_in,path_out,title,numpyA=float,saveA=True): # imports csv, converts to np array, and saves it as .npy file
-    #path_in = str(path_in)
-    #path_out = str(path_out)
-    #title = str(title)
+def csv_2_numpy(path_in,title,path_out='',dtype=np.float64,title_row=False):
+    """ opens up a .csv file and converts it to nupmpy
+
+    Parameters
+    ----------
+    path_in:    pathway to csv                          - str
+    title:      name of file                            - str
+    path_out:   ** if != '', will save to this pathway  - default = ''
+    dtype:      ** data type of numpy array             - default = np.float64
+    title_row:  ** if == True will skip a row           - default = False
+
+    Returns
+    -------
+    np.array
+    """
+
+    if path_in[-1] != '/':                              path_in  += '/'
+    if title[-4] != '.csv':                             title    += '.csv'
+    if all(( path_out != '' , path_out[-1] != '/' )):   path_out += '/'
+
     DATA = []
-    with open(path_in+title+".csv", newline='', encoding='utf-8') as d:
+    with open(path_in+title, newline='', encoding='utf-8') as d:
         reader = csv.reader(d)
         for row in reader:
             DATA.append(row)
-    DATA = np.array(DATA).astype(numpyA)
-    if saveA == True:
-        np.save(path_out+title+".npy",DATA)
+
+    if title_row:
+        DATA = np.array(DATA[1:]).astype(dtype)
     else:
-        return DATA
+        DATA = np.array(DATA).astype(dtype)
+
+    if path_out != '':
+        np.save(path_out+title+".npy",DATA)
+
+    return DATA
+
+def csv_2_pandas(path_in,title,path_out='',dtype=np.float64,title_row=False,keys=[]):
+    """ opens a .csv file and as a panda data frame
+
+    Parameters
+    ----------
+    path_in:    pathway to csv                          - str
+    title:      name of file                            - str
+    path_out:   ** if != '', will save to this pathway  - default = ''
+    dtype:      ** data type of numpy array             - default = np.float64
+    title_row:  ** if == True will be column names      - default = False
+    keys:       ** if len(keys)>0 will be column names  - default = []
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+
+    # check parameters
+    assert any(( len(keys) > 0 , title_row )), "either use title row as names or use 'keys'"
+
+    if path_in[-1] != '/':                              path_in     += '/'
+    if title[-4:] != '.csv':                            title       += '.csv'
+    if all(( path_out != '' , path_out[-1] != '/' )):   path_out    += '/'
+
+    # determine column names
+    if title_row == False:
+        # assert len(keys) == len(DATA[0,:]), "length of keys must equal number of colums!"
+        df = pd.read_csv(path_in+title,names=keys)
+
+    else:
+        df = pd.read_csv(path_in+title)
+
+    if path_out != '':
+        pd.to_pickle(df,path_out+title)
+
+    return df
+
+# def open_data(path_in,path_out,title,import_type='csv',export_type='pandas',key=[],saveA=True):
+#
+#     try:
+#         if export_type == 'pandas':
+#             data = pd.read_pickle(path_out+title+'.pkl')
+#         elif export_type == 'numpy':
+#             data = np.load(path_out+title+'.npy')
+#     except:
+#         if all(( import_type == 'csv', export_type == 'pandas')):
+#             data = CSV(path_in,path_out,title,saveA=saveA,key=key)
+#         elif all(( import_type == 'csv', export_type == 'numpy' )):
+#             data = CSV(path_in,path_out,title,saveA=saveA)
+#
+#     return data
+
+
 
 def sort(Array,column): # sorts array by given column number
     return Array[Array[:,column].argsort()]
@@ -28,7 +104,7 @@ def nearest(Array1,value): # finds the index of the nearest element in an array 
 
 def maxima(X,Y,x_approx,dx,ret='i_max'):# finds local maximum of array
     dx = int(dx)
-    i_approx = nearest(X,x_approx) # good 
+    i_approx = nearest(X,x_approx) # good
     i_add = len(X[:i_approx-dx])
     X_n = np.array(X[i_approx-dx:i_approx+dx+1])
     Y_n = np.array(Y[i_approx-dx:i_approx+dx+1])
@@ -61,7 +137,7 @@ def minima(X,Y,x_approx,dx,ret='i_min'): # finds local minimum of an array
         return x_min
     if ret == 'all':
         return (i_min+i_add),x_min,y_min
-    
+
 def extrema(X,Y,x_range,xmin_approx,xmax_approx,ret='sep',sortcol=0): # finds the exact extrema given a list of approximate minima and maxima
     dx = int(x_range)
     i_min,i_max = [],[]
@@ -87,7 +163,7 @@ def extrema(X,Y,x_range,xmin_approx,xmax_approx,ret='sep',sortcol=0): # finds th
         minima.append([x_min[i],y_min[i]])
     for i in range(len(x_max)):
         maxima.append([x_max[i],y_max[i]])
-        
+
     if ret == 'sep':
         return minima,maxima
     else:
@@ -119,20 +195,20 @@ def int_prec(x,p,err=False):
     return x_new
 
 class var:
-    def __init__(self,val,err,units):
+    def __init__(self,val,err):
         self.val = val
         self.err = err
-        self.units = units
+        # self.units = units
 
         try: # try for scalar values in val and err
 
             if self.err < 1:
-                
+
                 p = float_prec(self.err)
                 self.p = p
                 self.pval = round(self.val,p)
                 self.perr = round(self.err,p)
-                
+
             else:
 
                 p = len(str(int(self.err)))
@@ -166,15 +242,14 @@ class var:
                         self.p[i] = p
                         self.pval[i] = int_prec(self.val[i],p)
                         self.perr[i] = int_prec(self.err[i],p,err=True)
-                        
+
                 self.p = np.array(self.p).astype(int)
 
             except:
                 print("try something else")
-        
+
 def printvar(var):
     print(var.name,var.av,var.err,var.units)
 
 def printerr(var):
     print(var.name,var.err,var.err/var.val)
-
